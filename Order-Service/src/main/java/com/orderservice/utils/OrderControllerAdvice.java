a99fc3294b38807d8cc12e39bd6eb79a06ff6d3a
YunYunoy <seba12mik@interia.pl>
2023-06-11 22:38:33 +0200

    complete validation and exception handling for {User,Product,Order,Inventory}_Service

--- Order-Service/src/main/java/com/orderservice/utils/OrderControllerAdvice.java
+++ Order-Service/src/main/java/com/orderservice/utils/OrderControllerAdvice.java
@@ -2,12 +2,10 @@ package com.orderservice.utils;
 
 import com.orderservice.controller.OrderController;
 import lombok.Data;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestControllerAdvice(assignableTypes = OrderController.class)
@@ -34,15 +31,9 @@ public class OrderControllerAdvice {
     }
 
     @ExceptionHandler(InventoryException.class)
-    public ResponseEntity<String> handleInventoryException(MethodArgumentNotValidException ex) {
-        BindingResult bindingResult = ex.getBindingResult();
-        List<ObjectError> errors = bindingResult.getAllErrors();
-        List<String> errorMessages = errors.stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-        String errorMessage = String.join(", ", errorMessages);
+    public ResponseEntity<String> handleInventoryException(InventoryException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("Validation error: " + errorMessage);
+                .body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
